e6c7ae920210bf6196eb747f87f95b07b79a0df5
GathangaElijah <elyg3672@gmail.com>
2025-06-27 16:41:59 +0300

    feat: Added a hook to render the tasks. Also imported nanoid to give unique ids to the task

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,31 +1,58 @@
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-
+import { useState } from "react";
+import { nanoid } from "nanoid";
 // Filter buttons
 const FILTER_TYPE = ["All", "Active", "Completed"]
-  
+
+
 function App(props) {
+  // Tasks hook
+  const[tasks, setTasks] = useState(props.tasks);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
   // Tasklist
-  const taskList = props.tasks?.map((task) => (
+  const taskList = tasks?.map((task) => (
     <Todo 
     id={task.id} 
     name={task.name}
     completed={task.completed} 
     key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
     />
   ) );
+
+  // Adding a task to the list from the form.
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun}
+  remaining`;
   
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      {/* Form Component */}
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         {FILTER_TYPE.map((name) => (
           <FilterButton key={name} filterType={name} />
         ))};
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading"> {headingText} </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
