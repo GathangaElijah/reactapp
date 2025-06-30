d86744801bbf08814993bd4565761fc24b8999e6
GathangaElijah <elyg3672@gmail.com>
2025-06-30 21:49:50 +0300

    feat:  added accessibility features

--- moz-todo-react/src/components/Todo.jsx
+++ moz-todo-react/src/components/Todo.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 // This functions renders all the tasks
 function Todo(props){
   // Hooks
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
 
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
   // Listen for editing
   function handleChange(e) {
     setNewName(e.target.value);
@@ -32,6 +42,7 @@ function Todo(props){
         type="text" 
         value={newName}
         onChange={handleChange}
+        ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -64,9 +75,11 @@ function Todo(props){
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => 
-          setEditing(true)}>
-          Edit <span className="visually-hidden">{props.name}</span>
+        <button 
+        type="button" 
+        className="btn" onClick={() => setEditing(true)}
+        ref={editButtonRef}>
+        Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
           type="button"
@@ -78,6 +91,13 @@ function Todo(props){
     </div>
   );
  
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing){
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
   return (
       <li className="todo"> {isEditing ? editingTemplate : 
       viewTemplate} </li>
